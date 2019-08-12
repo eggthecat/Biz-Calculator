d90e455f8e532ad881b92f6ac7e8bf9820bae9d5
dominic montelongo and nikhil batta <nikhil@>
2019-08-12 11:09:04 -0700

    Add BMI function

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,35 @@
-var add = function(number1, number2){
-return number1 + number2
+//BUSINESS CALCULATOR
+// var add = function(number1, number2){
+// return number1 + number2
+// }
+//
+// var subtract = function (number1, number2) {
+//   return number1 - number2
+// }
+//
+// var multiply = function (number1, number2) {
+//   return number1 * number2
+// }
+//
+// var divide = function (number1, number2) {
+//   return number1 / number2
+// }
+//
+//
+// var number1 = parseInt(prompt("Enter a number:"))
+// var number2 = parseInt(prompt("Enter another number:"))
+// var result = (add(number1,number2))
+// var result2 = (subtract(number1,number2))
+// var result3 = (multiply(number1,number2))
+// var result4 = (divide(number1,number2))
+// alert(result + ", " + result2 + ", " + result3 + ", & " + result4);
+// BMI Calculator
+var bmi = function(number1, number2){
+  return number1 / (number2 * 2)
 }
 
-var subtract = function (number1, number2) {
-  return number1 - number2
-}
+var number1 = parseInt(prompt("Enter your weight in kilograms(don't lie):"))
+var number2 = parseFloat(prompt("Enter your height in meters:"))
 
-var multiply = function (number1, number2) {
-  return number1 * number2
-}
-
-var divide = function (number1, number2) {
-  return number1 / number2
-}divide
-
-
-var number1 = parseInt(prompt("Enter a number:"))
-var number2 = parseInt(prompt("Enter another number:"))
-var result = (add(number1,number2))
-alert(result);
+var result = (bmi(number1,number2))
+alert(result)
