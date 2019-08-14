5625672791112bbd5f4e136173ad2e9727d1abad
dominic montelongo <dominic@>
2019-08-14 14:22:04 -0700

    Add jquery-3.4.1, bootstrap, and uncomment calc functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,31 @@
-//BUSINESS CALCULATOR
-// var add = function(number1, number2){
-// return number1 + number2
-// }
-//
-// var subtract = function (number1, number2) {
-//   return number1 - number2
-// }
-//
-// var multiply = function (number1, number2) {
-//   return number1 * number2
-// }
-//
-// var divide = function (number1, number2) {
-//   return number1 / number2
-// }
-//
-//
-// var number1 = parseInt(prompt("Enter a number:"))
-// var number2 = parseInt(prompt("Enter another number:"))
-// var result = (add(number1,number2))
-// var result2 = (subtract(number1,number2))
-// var result3 = (multiply(number1,number2))
-// var result4 = (divide(number1,number2))
-// alert(result + ", " + result2 + ", " + result3 + ", & " + result4);
-// BMI Calculator
+BUSINESS CALCULATOR
+var add = function(number1, number2){
+return number1 + number2
+}
+
+var subtract = function (number1, number2) {
+  return number1 - number2
+}
+
+var multiply = function (number1, number2) {
+  return number1 * number2
+}
+
+var divide = function (number1, number2) {
+  return number1 / number2
+}
+
+
+var number1 = parseInt(prompt("Enter a number:"))
+var number2 = parseInt(prompt("Enter another number:"))
+var result = (add(number1,number2))
+var result2 = (subtract(number1,number2))
+var result3 = (multiply(number1,number2))
+var result4 = (divide(number1,number2))
+alert(result + ", " + result2 + ", " + result3 + ", & " + result4);
+
+
+BMI Calculator
 var bmi = function(number1, number2){
   return number1 / (number2 * 2)
 }
