708d74106c799599c842448e4cf250a9e6e06b6c
dominic montelongo <dominic@>
2019-08-14 15:47:56 -0700

    Add jQuery properties to calculator add function and remove prompt function

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,27 @@
 // BUSINESS CALCULATOR
+// Business (or back-end) logic
 var add = function(number1, number2){
-return number1 + number2
-}
+return number1 + number2;
+};
 
 var subtract = function (number1, number2) {
-  return number1 - number2
-}
+  return number1 - number2;
+};
 
 var multiply = function (number1, number2) {
-  return number1 * number2
-}
+  return number1 * number2;
+};
 
 var divide = function (number1, number2) {
-  return number1 / number2
-}
-
-
-var number1 = parseInt(prompt("Enter a number:"))
-var number2 = parseInt(prompt("Enter another number:"))
-var result = (add(number1,number2))
-var result2 = (subtract(number1,number2))
-var result3 = (multiply(number1,number2))
-var result4 = (divide(number1,number2))
-alert(result + ", " + result2 + ", " + result3 + ", & " + result4);
-
-
-// BMI Calculator
-var bmi = function(number1, number2){
-  return number1 / (number2 * 2)
-}
-
-var number1 = parseInt(prompt("Enter your weight in kilograms(don't lie):"))
-var number2 = parseFloat(prompt("Enter your height in meters:"))
-
-var result = (bmi(number1,number2))
-alert(result)
+  return number1 / number2;
+};
+// BUSINESS CALCULATOR
+// Interface (or front-end) logic
+$(document).ready(function(){
+  $("form#add").submit(function(Event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    alert(add(number1, number2));
+  })
+});
